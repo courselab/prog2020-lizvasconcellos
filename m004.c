f85a11dc1dfdf7dadccd029428bebf1cc1763886
lizvasconcellos <liz.vasconcellos@usp.br>
2020-07-05 18:26:26 +0000

    entrega m004

--- m004.c
+++ m004.c
@@ -27,12 +27,40 @@
    right before the spurious character (eg. truncate 110x001
    into 110); an empty string means zero. */
 
+int potencia (int base, int exp)
+{
+  int total = 1;
+
+  if (exp == 0)
+  {
+    total = total;
+  }
+  else
+  {
+      for (int i = 0 ; i < exp ; i++)
+    {
+      total = total * base;
+    }
+  }
+  
+  return total;
+}
+
+
 int decimal (char *b)
 {
-  
-  n = strlen (b)
 
-  return 0;
+  int n = strlen (b);
+
+  int soma = 0;
+
+  for (int i = n - 1 ; i >= 0 ; i--)
+  {
+    if ( b[i] == '1' )
+    soma = soma + potencia (2, i);
+  }
+
+  return soma;
 }
 
 #define USAGE "m004 <string>\n"
