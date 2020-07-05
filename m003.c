29728ad92dd52c5e8d6d04ec5e53cbbeb373dcb7
lizvasconcellos <liz.vasconcellos@usp.br>
2020-07-05 01:47:19 +0000

    entrega(1) m003

--- m003.c
+++ m003.c
@@ -33,17 +33,22 @@ int is_prime (int a)
     {
       ++x;
     }
-    else
-    ;
   }
 
-  if ( x > 2)
+  if ( x > 2 )
   {
     return 0;
   }
   else
   {
-    return 1;
+    if (x == 1)
+    {
+      return 0;
+    }
+    else
+    {
+      return 1;
+    }
   }
   
 }
